cf16c43ab65da641b7e2e0e5c89deeadafa910cf
Francisco Geovan Queiroga de Assis Netto <francis.netto2011@gmail.com>
2022-11-12 13:10:24 -0300

    Projeto atualizado totalmente funcional

--- APImysql/Program.cs
+++ APImysql/Program.cs
@@ -3,22 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 var connectionStringMysql = builder.Configuration.GetConnectionString("ConnectionMysql");
 builder.Services.AddDbContext<APIDbContext>(options => options.UseMySql (
     connectionStringMysql,
-    ServerVersion.Parse("3306-localhost")
+    ServerVersion.Parse("8.0.31")
     )
 );
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
